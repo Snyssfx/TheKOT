867ade7dac4bf965d7a81cca3de46c016750711c
Nik Tarasenko <nik.d.tarasenko@gmail.com>
2017-04-01 19:54:16 +0300

    - Added first script files

---
+++ TheKOT/Assets/IController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+	internal abstract class IController
+	{
+		private int XVector;
+		private int YVector;
+		private float Speed;
+
+		public abstract void Move();
+	}
+}
---
+++ TheKOT/Assets/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+	internal class PlayerController : IController
+	{
+		public override void Move()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
