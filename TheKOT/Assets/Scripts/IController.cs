fb54fcfa6c4c229a3fc0e59e6aa3e00dd5592aa8
Nik Tarasenko <nik.d.tarasenko@gmail.com>
2017-04-02 02:52:35 +0300

    - Conflicts has been resolved

--- TheKOT/Assets/Scripts/IController.cs
+++ TheKOT/Assets/Scripts/IController.cs
@@ -18,5 +18,7 @@ namespace Assets
 		public abstract void Control(GameObject gameObject);
 
 		internal abstract void Move(GameObject gameObject);
+
+		internal abstract void Rotate(GameObject gameObject);
 	}
 }
