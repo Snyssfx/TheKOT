e65c3af330ad0c7662b9440cfa271bbe22878a6f
Snyssfx <snyssfx@gmail.com>
2017-04-02 09:12:08 +0300

    -for merch

--- TheKOT/Assets/UpdateEveryFrame.cs
+++ TheKOT/Assets/UpdateEveryFrame.cs
@@ -2,20 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UpdateEveryFrame : MonoBehaviour
-{
+public class UpdateEveryFrame : MonoBehaviour {
 
 	// Use this for initialization
-	void Start ()
-	{
+	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update ()
-	{
-		var q = gameObject.transform.rotation;
-		q.eulerAngles = new Vector3(q.eulerAngles.x, q.eulerAngles.y, 0.0f);
-		gameObject.transform.rotation = q;
+	void Update () {
+    var q = gameObject.transform.rotation;
+    q.eulerAngles = new Vector3(q.eulerAngles.x, q.eulerAngles.y, 0.0f);
+    gameObject.transform.rotation = q;
+    
 	}
 }
